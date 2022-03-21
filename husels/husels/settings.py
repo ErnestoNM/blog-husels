ceda3c158d74c4ef17f7a8e21aaf3450ff20770a
ErnestoNM <ernestonm90@gmail.com>
2022-03-21 10:45:31 -0600

    blog básico funcionando

--- husels/husels/settings.py
+++ husels/husels/settings.py
@@ -15,12 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIRS = BASE_DIR / 'templates'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qq++ctqgox@3f!z33c$@jwd9st^vd7oo%ra9d7a48)px@!9%%b'
+SECRET_KEY = 'django-insecure-(2@xi8s6)b5%c55^86r(dz)a%x#3#&1h+snsk-f0426u#=d(5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'husels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
